af7686ae56d5c3142f3a11e1d666d3cb07d91fd3
Hamish Self <hsel0001@student.monash.edu>
2019-02-02 15:33:40 +1100

    Modified import script to work with new data format.
    Results.csv also modified by adding a duplicate row for testing 
    purposes.

--- CFD analysis/import_cfd.py
+++ CFD analysis/import_cfd.py
@@ -5,26 +5,53 @@ import matplotlib.pyplot as plt
 
 filename = './results/results.csv'
 
+def restoreNumericList(list_str):
+  lst = list_str.replace('[', '').replace(']', '').split(', ')
+  lst = [float(element) for element in lst]
+  return lst
+
+def restore2DList(arr_str):
+  arr = [restoreNumericList(vector_str) for vector_str in arr_str]
+  return arr
+
+def restore3DList(arr_str):
+  # for lst in arr_str:
+  #   print(lst)
+  arr = [list2d for list2d in arr_str]
+  new_arr = []
+  for i in range(0,len(arr)):
+    arr[i] = arr[i].replace('[[','[').replace(']]',']')
+    # will not work if 'xx' is in an array
+    sublists = [restoreNumericList(lst) for lst in arr[i].replace(', [','xx[').split('xx')]
+    new_arr.append(sublists)
+  return new_arr
+
 def readCFDData(file_name):
   # READ THE CSV
   with open(file_name, 'r') as csv_file:
-    reader  = csv.reader(csv_file)
+    reader  = csv.reader(csv_file, delimiter=',')
     data = np.array([row for row in reader])
   csv_file.close()
   # EXTRACT DATA
-  data = data[1:, 2:]
+  headers = data[0, :]
+  data = data[1:, 2:] # ignore header, rocket, and launch location
+  restore3DList(data[:, 8])
   data_dict = {
-    'mach numbers': data[:, 0],
-    'pitches': data[:, 1],
-    'rolls': data[:, 2],
-    'drag vectors': data[:, 3:6],
-    'total drags': data[:, 6],
-    'coeff vectors': data[:, 7:10],
-    'total coeffs': data[:, 10]
+    'mach numbers': data[:, 0].astype(float),
+    'pitches': data[:, 1].astype(float),
+    'rolls': data[:, 2].astype(float),
+    'drag vectors': np.array(restore2DList(data[:, 3])),
+    'total drags': data[:, 4].astype(float),
+    'c_d vectors': np.array(restore2DList(data[:, 5])),
+    'total coeffs': data[:, 6].astype(float),
+    'moment x locations': np.array(restore2DList(data[:, 7])),
+    'moments': np.array(restore3DList(data[:, 8])),
+    'c_m array': np.array(restore3DList(data[:, 9]))
     }
   return data_dict
 
 data_obj = readCFDData(filename)
+# print(data_obj['c_m array'].shape)
 
 # # INTERPOLATE
 # x = np.empty((len(mach_num),3))
